c662b253c8fa8ad072f9e9ecfffb88f0be982ce0
kboul <kos.voulgaridis@gmail.com>
2019-06-29 23:43:23 +0300

    Remove comments from environmetn.ts & add command for running ng serve with prod argument

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+// Run `ng serve -c production` to use environmental variables
+// stored in environment.prod.ts
+
 export const environment = {
     production: true,
     navBarBackgroundColor: 'red'
